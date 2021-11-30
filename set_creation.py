732a698c6a4c105d14ea612897e2996492a526cd
Bobby Hutter <bobby-h@Bobbys-MacBook-Pro.local>
2021-11-29 21:16:18 -0500

    added set comprehension for genre abbreviations

--- set_creation.py
+++ set_creation.py
@@ -1,3 +1,4 @@
 genre_results = ['rap', 'classical', 'rock', 'rock', 'country', 'rap', 'rock', 'latin', 'country', 'k-pop', 'pop', 'rap', 'rock', 'k-pop',  'rap', 'k-pop', 'rock', 'rap', 'latin', 'pop', 'pop', 'classical', 'pop', 'country', 'rock', 'classical', 'country', 'pop', 'rap', 'latin', 'latin']
 
 survey_genres = set(genre_results)
+survey_abbreviated = {genre[0:3] for genre in genre_results}
