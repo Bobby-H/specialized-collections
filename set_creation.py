a043f8148c40a0b8416aea8656321fdfb8933e40
Bobby Hutter <bobby-h@Bobbys-MacBook-Pro.local>
2021-11-29 20:07:06 -0500

    branched off main; CC code sample

--- set_creation.py
+++ set_creation.py
@@ -1,3 +1,3 @@
-genre_results = ['rap', 'classical', 'rock', 'rock', 'country', 'rap', 'rock', 'latin', 'country', 'k-pop', 'pop', 'rap', 'rock', 'k-pop',  'rap', 'k-pop', 'rock', 'rap', 'latin', 'pop', 'pop', 'classical', 'pop', 'country', 'rock', 'classical', 'country', 'pop', 'rap', 'latin']
+genre_results = ['rap', 'classical', 'rock', 'rock', 'country', 'rap', 'rock', 'latin', 'country', 'k-pop', 'pop', 'rap', 'rock', 'k-pop',  'rap', 'k-pop', 'rock', 'rap', 'latin', 'pop', 'pop', 'classical', 'pop', 'country', 'rock', 'classical', 'country', 'pop', 'rap', 'latin', 'latin']
 
 # Write your code below!
