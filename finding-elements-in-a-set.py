8c456d3ab5a42cf6d76788357fb10039903ad659
Bobby Hutter <bobby-h@Bobbys-MacBook-Pro.local>
2021-12-06 07:33:57 -0500

    completed code

--- finding-elements-in-a-set.py
+++ finding-elements-in-a-set.py
@@ -2,4 +2,20 @@ allowed_tags = ['pop', 'hip-hop', 'rap', 'dance', 'electronic', 'latin', 'indie'
 
 song_data_users = {'Retro Words': ['pop', 'explosion', 'hammer', 'bomb', 'warm', 'due', 'writer', 'happy', 'horrible', 'electric', 'mushroom', 'shed']}
 
-# Write your code below!
+# checkpoint 1
+tag_set = set(song_data_users['Retro Words'])
+
+# checkpoint 2
+bad_tags =[]
+for tag in tag_set:
+  if tag not in allowed_tags:
+    bad_tags.append(tag)
+
+# checkpoint 3
+for tag in bad_tags:
+  if tag in tag_set:
+    tag_set.remove(tag)
+
+# checkpoint 4
+song_data_users['Retro Words'] = tag_set
+print(tag_set)
